b82d0876802c7e41f5ffcff5fbed1c772e06717c
Roman Micuda <roman.micuda@outlook.com>
2024-12-01 19:52:44 +0100

    feat: add image recognition tab and screen; remove unused components

--- app/(tabs)/imageRecognition.tsx
+++ app/(tabs)/imageRecognition.tsx
@@ -1,15 +1,14 @@
 import { Stack } from "expo-router";
-import { StyleSheet, Image, Platform } from "react-native";
 import { Text, View } from "react-native";
 
-export default function TabTwoScreen() {
+export default function imageRecognition() {
   return (
     <View>
       <View>
         <Stack.Screen
           options={{
             headerShown: true,
-            title: "Expore Recipes",
+            title: "Image Recognition",
           }}
         />
       </View>
